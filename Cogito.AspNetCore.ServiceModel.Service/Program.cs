86cc4fc85af833890c0658ca901bb492c9497269
Jerome Haltom <jhaltom@alethic.solutions>
2020-08-07 19:20:22 -0500

    Move src around.

--- Cogito.AspNetCore.ServiceModel.Service/Program.cs
+++ Cogito.AspNetCore.ServiceModel.Service/Program.cs
@@ -4,19 +4,16 @@ using Microsoft.AspNetCore.Hosting;
 namespace Cogito.AspNetCore.ServiceModel.Service
 {
 
-    public class Program
+    public static class Program
     {
 
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build()
+                .Run();
+        }
 
     }
 
